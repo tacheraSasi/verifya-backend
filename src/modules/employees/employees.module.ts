655fe24276c3175158ca82b82b02bde76b7259a5
tacherasasi <tacherasasi@gmail.com>
2025-07-14 23:40:15 +0300

    Add NotificationsModule import to EmployeesModule

--- src/modules/employees/employees.module.ts
+++ src/modules/employees/employees.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
