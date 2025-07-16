b65e8a9c4084b29508af618e77b0d5b7fbfb0044
tacherasasi <tacherasasi@gmail.com>
2025-07-16 14:42:27 +0300

    more

--- src/modules/offices/entities/office.entity.ts
+++ src/modules/offices/entities/office.entity.ts
@@ -1,31 +1,18 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  OneToMany,
-  OneToOne,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, Column, OneToMany, OneToOne, JoinColumn } from 'typeorm';
 import { User } from 'src/modules/users/entities/user.entity';
 import { Attendance } from 'src/modules/attendances/entities/attendance.entity';
+import { BasicEntity } from 'src/common/entities/base.entity';
 
 @Entity('offices')
-export class Office {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
+export class Office extends BasicEntity {
   @Column()
   name: string;
 
-  @Column('decimal', { precision: 10, scale: 7, nullable:true })
+  @Column('decimal', { precision: 10, scale: 7, nullable: true })
   latitude: number;
 
-  @Column('decimal', { precision: 10, scale: 7, nullable:true})
+  @Column('decimal', { precision: 10, scale: 7, nullable: true })
   longitude: number;
-
-  @CreateDateColumn()
-  createdAt: Date;
 
   @OneToOne(() => User, { eager: true })
   @JoinColumn()
