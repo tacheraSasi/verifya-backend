005081f14d17276ad74839a6cfd6f31ecf2adde9
Tachera Sasi <tacherasasi@gmail.com>
2025-09-25 17:02:52 +0300

    Refactor OfficesModule to include TypeOrmModule for Office, Employee, and Attendance entities

--- src/modules/offices/offices.module.ts
+++ src/modules/offices/offices.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfficesService } from './offices.service';
 import { OfficesController } from './offices.controller';
 import { UsersModule } from '../users/users.module';
+import { Office } from './entities/office.entity';
+import { Employee } from '../employees/entities/employee.entity';
+import { Attendance } from '../attendances/entities/attendance.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Office, Employee, Attendance]),
+    UsersModule,
+  ],
   controllers: [OfficesController],
   providers: [OfficesService],
 })
